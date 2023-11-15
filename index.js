2bee2c286493e6ae03454aae960ea24d47437820
Akthab <mhmdakthab6@gmail.com>
2023-11-16 00:10:23 +0530

    feat(): updated the url_shortner function

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const dns = require('dns');
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const urls = {};
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -10,15 +15,46 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get('/', function (req, res) {
+	res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/hello', function (req, res) {
+	res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+app.post('/api/shorturl', (req, res) => {
+	var url = req.body.url;
+	const regex = /\/$/;
+	url = url.replace(regex, '');
+
+	if (url.startsWith('http://') || url.startsWith('https://')) {
+		const regex = /^https?:\/\/?/;
+		url = url.replace(regex, '');
+	}
+
+	dns.lookup(url, (err) => {
+		if (err) {
+			res.json({ error: 'invalid url' });
+		} else {
+			const shortUrl = Object.keys(urls).length + 1;
+			urls[shortUrl] = url;
+			res.json({ original_url: url, short_url: shortUrl });
+		}
+	});
+});
+
+app.get('/api/shorturl/:short_url', (req, res) => {
+	const shortUrl = req.params.short_url;
+	const url = urls[shortUrl];
+	if (url) {
+		res.redirect('https://' + url);
+	} else {
+		res.json({ error: 'short url not found' });
+	}
+});
+
+app.listen(port, function () {
+	console.log(`Listening on port ${port}`);
 });
